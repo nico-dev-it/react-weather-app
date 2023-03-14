2149cba19d09c1c8d754a9ace06ab723d86182af
Nicolas Herbreteau <nicolasherbreteau@nicolass-mbp.home>
2023-03-14 16:50:17 +0000

    form handlechange function

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,12 +1,29 @@
 import './weather.css'
+import {useState} from "react";
 
 const Weather = () => {
+
+    const [form, setForm] = useState({city: "", country:""})
+
+    const handleChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+
+        if (name == "city") {
+            setForm({...form, city: value})
+        }
+        if (name == "country") {
+            setForm({...form, country: value})
+        }
+        console.log(form.city)
+    }
+
     return (
         <div class="formContainer">
             <h1>Weather</h1>
             <form>
-                <input type="text" name="city" placeholder="city"/>
-                <input type="text" name="country" placeholder="country"/>
+                <input type="text" name="city" placeholder="city" onChange={event => handleChange(event)}/>
+                <input type="text" name="country" placeholder="country" onChange={event => handleChange(event)}/>
                 <button type="submit">Submit</button>
             </form>
         </div>
