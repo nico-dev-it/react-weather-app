5fe5d909cab634c43c527f7573f3b4a7f34bd464
Nicolas Herbreteau <nicolasherbreteau@nicolass-mbp.home>
2023-03-14 16:21:28 +0000

    basic styling

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,9 @@
+import './weather.css'
+
 const Weather = () => {
     return (
         <div class="formContainer">
+            <h1>Weather</h1>
             <form>
                 <input type="text" name="city" placeholder="city"/>
                 <input type="text" name="country" placeholder="country"/>
