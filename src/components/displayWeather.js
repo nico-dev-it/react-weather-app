47d67253a617d3197aa4f745a78092885d0c2370
Nicolas Herbreteau <nicolasherbreteau@nicolass-mbp.home>
2023-03-15 14:50:28 +0000

    more weather details

--- src/components/displayWeather.js
+++ src/components/displayWeather.js
@@ -5,12 +5,14 @@ const DisplayWeather = (props) => {
     return (
         <div className="weatherContainer">
             <div className="locationCard">
-                <p><span>{props.data.name}</span></p>
+                <p><span className="mainTitle">{props.data.name}</span></p>
                 <p><span>{props.data.sys.country}</span></p>
+                <p>{props.data.weather[0].description}</p>
             </div>
             <div className="mainWeatherCard">
-                <p><span>{Math.round(props.data.main.temp)}°</span></p>
-                <p><span>H:{Math.round(props.data.main.temp_max)}° L:{Math.round(props.data.main.temp_min)}°</span></p>
+                <p><span className="mainTitle">{Math.round(props.data.main.temp)}°</span></p>
+                <p><span>H: {Math.round(props.data.main.temp_max)}° L: {Math.round(props.data.main.temp_min)}°</span></p>
+                <p><span>Feels like: {Math.round(props.data.main.feels_like)}°</span></p>
             </div>
         </div>
     )
